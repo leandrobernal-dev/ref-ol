c2d7e85e1520851fbba9962be3f9840523250286
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-04 03:08:25 +0800

    Add ThemeProvider and ThemeToggler components

---
+++ src/theme/ThemeProvider.js
@@ -0,0 +1,12 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }) {
+    return (
+        <NextThemesProvider {...props} defaultTheme="dark">
+            {children}
+        </NextThemesProvider>
+    );
+}
