c36dde54a5753e9ee317fc06bc10f42987beeff2
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-04 03:08:04 +0800

    Add suppressHydrationWarning to html tag in RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className={`${inter.className}`} suppressHydrationWarning>
                 {children}
             </body>
