dc3ff01030520554a8b34668133386fd90fbc829
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-07 04:52:05 +0800

    Show file thumbnail if exists

--- src/app/(files)/components/FileCard.js
+++ src/app/(files)/components/FileCard.js
@@ -10,13 +10,23 @@ import { FileActionModal } from "@/app/(files)/components/FileActionModal";
 export function FileCard({ file }) {
     return (
         <Card className="w-full rounded-none">
-            <CardHeader className="p-0 relative cursor-pointer">
-                <Link href={"/file/" + file.id}>
-                    <img
-                        src="https://i.pinimg.com/564x/b5/2e/a9/b52ea99845a34aded25e4ae18d9f1e08.jpg"
-                        alt=""
-                    />
-                </Link>
+            <CardHeader className="p-0 relative aspect-square">
+                {file.thumbnail ? (
+                    <Link href={"/file/" + file.id}>
+                        <img src={file.thumbnail} alt="" />
+                    </Link>
+                ) : (
+                    <Link
+                        className="w-full h-full grid grid-cols-2 gap-1 p-4"
+                        href={"/file/" + file.id}
+                    >
+                        <div className="w-full h-full outline outline-1"></div>
+                        <div className="grid grid-rows-2 gap-1">
+                            <div className="w-full h-full outline outline-1"></div>
+                            <div className="w-full h-full outline outline-1"></div>
+                        </div>
+                    </Link>
+                )}
                 <div className="absolute top-0 right-2 flex flex-col gap-1">
                     <DeleteFileModal file={file} />
 
