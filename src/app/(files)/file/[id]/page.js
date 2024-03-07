9fe7abcbcfd1bf2236591245203a0970deadedc8
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-08 01:08:06 +0800

    Add save functionality to FilePageLayout

--- src/app/(files)/file/[id]/page.js
+++ src/app/(files)/file/[id]/page.js
@@ -1,22 +1,46 @@
 "use client";
 
+import { updateFileImage } from "@/app/(files)/actions/update";
 import AddLoader from "@/app/(files)/file/components/AddLoader";
 import Canvas from "@/app/(files)/file/components/Canvas";
 import ContextMenuProvider from "@/app/(files)/file/components/ContextMenu";
 import Loader from "@/app/(files)/file/components/Loader";
 import { FileContext } from "@/app/(files)/file/context/FileContext";
 import { Button } from "@/components/ui/button";
-import { Cross1Icon } from "@radix-ui/react-icons";
+import { Cross1Icon, ReloadIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { useContext, useState } from "react";
 
 export default function FilePageLayout() {
     const [isAdding, setIsAdding] = useState(false);
+    const [isSaving, setIssaving] = useState(false);
     const [addingProgress, setAddingProgress] = useState({
         finished: 0,
         total: 0,
     });
-    const { progress } = useContext(FileContext);
+    const { progress, updatedElements, elements, setUpdatedelements } =
+        useContext(FileContext);
+
+    async function handleSave() {
+        setIssaving(true);
+        const elementsToUpdate = elements
+            .filter((el) => updatedElements.includes(el.id))
+            .map((el) => {
+                return {
+                    id: el.id,
+                    transform: {
+                        x: el.x,
+                        y: el.y,
+                        width: el.width,
+                        height: el.height,
+                        rotationAngle: el.rotationAngle,
+                    },
+                };
+            });
+        await updateFileImage(JSON.stringify(elementsToUpdate));
+        setIssaving(false);
+        setUpdatedelements([]);
+    }
     return (
         <>
             {progress.finished === progress.total ? (
@@ -27,15 +51,27 @@ export default function FilePageLayout() {
                                 setAddLoaderOpen={setIsAdding}
                                 setAddLoaderProgress={setAddingProgress}
                             />
-                            <Link href={"/files"}>
-                                <Button
-                                    variant="outline"
-                                    className="z-20 absolute right-2 top-2"
-                                    size="icon"
-                                >
-                                    <Cross1Icon className="h-4 w-4" />
-                                </Button>
-                            </Link>
+                            <div className="absolute flex gap-2 items-center right-2 top-2">
+                                {updatedElements.length > 0 && (
+                                    <Button
+                                        type="submit"
+                                        onClick={handleSave}
+                                        disabled={isSaving}
+                                    >
+                                        {isSaving ? (
+                                            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                                        ) : null}
+                                        {isSaving
+                                            ? "Saving..."
+                                            : "Save Changes"}
+                                    </Button>
+                                )}
+                                <Link href={"/files"}>
+                                    <Button variant="outline" size="icon">
+                                        <Cross1Icon className="h-4 w-4" />
+                                    </Button>
+                                </Link>
+                            </div>
                         </div>
                     </ContextMenuProvider>
                     <AddLoader open={isAdding} progress={addingProgress} />
--- src/app/(files)/file/context/FileContext.js
+++ src/app/(files)/file/context/FileContext.js
@@ -1,16 +1,33 @@
 "use client";
 import ImageElement from "@/app/(files)/file/classes/ImageElement";
+import useHistory, { SelectCommand } from "@/app/(files)/file/hooks/useHistory";
 import { createContext, useEffect, useState } from "react";
 
 export const FileContext = createContext();
 
 export default function FileContextProvider({ children, images }) {
+    const [updatedElements, setUpdatedelements] = useState([]);
     const [elements, setElements] = useState([]);
     const [progress, setProgress] = useState({
         total: images.length,
         finished: 0,
     });
     const { executeCommand, undo, redo, history, currentIndex } = useHistory();
+
+    useEffect(() => {
+        const historyWithoutSelect = history.filter(
+            (command) => !(command instanceof SelectCommand)
+        );
+
+        if (historyWithoutSelect.length > 0) {
+            const changedElementsIds = historyWithoutSelect
+                .flatMap((command) => command.elementIds)
+                .filter((value, index, self) => self.indexOf(value) === index);
+            setUpdatedelements(changedElementsIds);
+        } else {
+            setUpdatedelements([]);
+        }
+    }, [history, currentIndex]);
 
     useEffect(() => {
         async function createImageElements() {
@@ -21,7 +38,10 @@ export default function FileContextProvider({ children, images }) {
                     imageData.transform.x,
                     imageData.transform.y,
                     false,
-                    imageData.id
+                    imageData.id,
+                    imageData.transform.width,
+                    imageData.transform.height,
+                    imageData.transform.rotationAngle
                 );
                 await imageElement.create();
                 imageElements.push(imageElement);
@@ -48,6 +68,8 @@ export default function FileContextProvider({ children, images }) {
                 executeCommand,
                 undo,
                 redo,
+                updatedElements,
+                setUpdatedelements,
             }}
         >
             {children}
