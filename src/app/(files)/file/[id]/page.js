c5d15eb3ca23eccfcd3efbfb101196eba8dd4ff2
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-07 15:13:47 +0800

    Add button to close canvas

--- src/app/(files)/file/[id]/page.js
+++ src/app/(files)/file/[id]/page.js
@@ -5,6 +5,9 @@ import Canvas from "@/app/(files)/file/components/Canvas";
 import ContextMenuProvider from "@/app/(files)/file/components/ContextMenu";
 import Loader from "@/app/(files)/file/components/Loader";
 import { FileContext } from "@/app/(files)/file/context/FileContext";
+import { Button } from "@/components/ui/button";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 import { useContext, useState } from "react";
 
 export default function FilePageLayout() {
@@ -19,10 +22,21 @@ export default function FilePageLayout() {
             {progress.finished === progress.total ? (
                 <>
                     <ContextMenuProvider>
-                        <Canvas
-                            setAddLoaderOpen={setIsAdding}
-                            setAddLoaderProgress={setAddingProgress}
-                        />
+                        <div className="relative">
+                            <Canvas
+                                setAddLoaderOpen={setIsAdding}
+                                setAddLoaderProgress={setAddingProgress}
+                            />
+                            <Link href={"/files"}>
+                                <Button
+                                    variant="outline"
+                                    className="z-20 absolute right-2 top-2"
+                                    size="icon"
+                                >
+                                    <Cross1Icon className="h-4 w-4" />
+                                </Button>
+                            </Link>
+                        </div>
                     </ContextMenuProvider>
                     <AddLoader open={isAdding} progress={addingProgress} />
                 </>
