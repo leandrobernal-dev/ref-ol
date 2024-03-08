a768fc8e44eb7bc0fa0f0a82315ce60216566e3b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-08 08:20:16 +0800

    add 500kb limit to image files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "500kb",
+        },
+    },
+};
 
 export default nextConfig;
