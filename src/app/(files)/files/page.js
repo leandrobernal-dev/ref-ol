0fc5715ad50afebf181119f2ba785a2e3a567f90
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-09 12:34:18 +0800

    Add theme toggle and logout button to FilesPage

--- src/app/(files)/files/page.js
+++ src/app/(files)/files/page.js
@@ -4,19 +4,33 @@ import { FileActionModal } from "@/app/(files)/components/FileActionModal";
 import { FileDataContext } from "@/app/(files)/context/FilesContext";
 import { useContext } from "react";
 import { Button } from "@/components/ui/button";
+import { ExitIcon } from "@radix-ui/react-icons";
+import { ThemeToggle } from "@/theme/ThemeToggler";
+import Link from "next/link";
 
 export default function FilesPage() {
     const { optimisticFiles: files } = useContext(FileDataContext);
     return (
         <main className="p-8">
-            <section>
-                <span>My Files</span> |{" "}
-                <span>
-                    <FileActionModal
-                        action={"create"}
-                        children={<Button variant="">Create new</Button>}
-                    />
-                </span>
+            <section className="flex justify-between">
+                <section>
+                    <span>My Files</span> |{" "}
+                    <span>
+                        <FileActionModal
+                            action={"create"}
+                            children={<Button variant="">Create new</Button>}
+                        />
+                    </span>
+                </section>
+                <section className="flex items-center gap-2">
+                    <ThemeToggle />
+                    <Link href="/auth/logout">
+                        <Button variant="outline">
+                            <ExitIcon className="mr-2 h-4 w-4" />
+                            Logout
+                        </Button>
+                    </Link>
+                </section>
             </section>
             {files.length === 0 ? (
                 <section className="text-center flex flex-col gap-4 justify-center h-full">
