0cdf7ab614c7ebcc77195ee2fd64461c2009e229
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-09 12:33:59 +0800

    Change request method from POST to GET in logout route

--- src/app/auth/logout/route.js
+++ src/app/auth/logout/route.js
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
+export async function GET(request) {
     const requestUrl = new URL(request.url);
     const cookieStore = cookies();
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
