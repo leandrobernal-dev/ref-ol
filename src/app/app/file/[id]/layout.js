5b96f2278e207991318f8775199842f70f4cba85
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-03-14 14:48:21 +0800

    Return 404 if file is not created by currentUser

--- src/app/app/file/[id]/layout.js
+++ src/app/app/file/[id]/layout.js
@@ -1,8 +1,11 @@
 import FileContextProvider from "@/app/app/file/context/FileContext";
 import Files from "@/models/Files";
 import Images from "@/models/Images";
+import User from "@/models/User";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) {
     const file = await Files.findOne({ _id: params.id });
@@ -10,25 +13,16 @@ export async function generateMetadata({ params }) {
         title: file.name + " - refOnline",
     };
 }
-async function getSignedUrlForKey(key) {
-    const s3Client = new S3Client({
-        region: process.env.AWS_REGION,
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        },
-    });
-    const url = await getSignedUrl(
-        s3Client,
-        new GetObjectCommand({
-            Bucket: "refol",
-            Key: key,
-        }),
-        { expiresIn: 60 }
-    );
-    return url;
-}
+
 export default async function FileLayout({ children, params }) {
+    const { user } = await getServerSession();
+    const currentUser = await User.findOne({ email: user.email });
+    const file = await Files.findOne({ _id: params.id });
+
+    if (file.created_by.toString() != currentUser._id.toString()) {
+        notFound(); // If the file is not created by the current user, return 404
+    }
+
     const s3Client = new S3Client({
         region: process.env.AWS_REGION,
         credentials: {
